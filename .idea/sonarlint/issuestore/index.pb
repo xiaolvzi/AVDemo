c761e4602c001cebccaf44614799c7cff9318d42
Robbit <robbit.lv@jollycorp.com>
2019-10-24 11:15:58 +0800

    init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+f
+6app/src/main/java/com/example/avdemo/MainActivity.java,4\5\4528821b822346c54cc2622f7a653386f4e317c3
---
+++ app/src/main/java/com/example/avdemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.avdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * desc: App入口 <br/>
+ * time: 2019/10/24 10:10 <br/>
+ * author: 吕昊臻 <br/>
+ * since V 1.0 <br/>
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
