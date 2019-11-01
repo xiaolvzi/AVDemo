85b28cf3db429ad2dd303a827c89e03015bcf802
Robbit <robbit.lv@jollycorp.com>
2019-11-01 10:45:33 +0800

    添加录制视频

--- app/src/main/java/com/example/avdemo/ui/video/capture/ActivityVideoCapture.kt
+++ app/src/main/java/com/example/avdemo/ui/video/capture/ActivityVideoCapture.kt
@@ -12,6 +12,14 @@ import java.io.IOException
 import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.media.CamcorderProfile
+import android.media.CamcorderProfile.QUALITY_HIGH
+import android.media.MediaRecorder
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.example.avdemo.R
+import java.io.File
 
 
 /**
@@ -22,70 +30,159 @@ import android.icu.lang.UCharacter.GraphemeClusterBreak.T
  */
 @Suppress("DEPRECATION")
 @Route(path = ActivityVideoCapture.Target)
-class ActivityVideoCapture : AppCompatActivity(), TextureView.SurfaceTextureListener {
-    private var previewIsRunning: Boolean = false
-    private val camera: Camera by lazy { Camera.open() }
+class ActivityVideoCapture : AppCompatActivity(), TextureView.SurfaceTextureListener, Camera.PreviewCallback, View.OnClickListener {
+
+    private lateinit var camera: Camera
+    private var surface: SurfaceTexture? = null
+    private var isTakingPicture: Boolean = false
+    private var isStopPreview: Boolean = false
+    private var isRecording: Boolean = false
+    private val mediaRecorder by lazy { MediaRecorder() }
 
     companion object {
         const val Target = ViewPathConst.ACTIVITY_VIDEO_CPATURE
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.e("lv", "onCreate")
         super.onCreate(savedInstanceState)
-        setContentView(com.example.avdemo.R.layout.activity_video_capture)
+        setContentView(R.layout.activity_video_capture)
         tv_camera_preview.surfaceTextureListener = this
+        initCamera()
+        initListener()
 
+    }
+
+    private fun initCamera() {
+        camera = Camera.open()
+        camera.setPreviewCallback(this)
+    }
+
+    private fun initListener() {
+        bt_take_picture.setOnClickListener(this)
+        bt_capture_video.setOnClickListener(this)
     }
 
     override fun onResume() {
+        Log.e("lv", "onResume")
         super.onResume()
-        startPreview()
+        camera.startPreview()
     }
 
     override fun onPause() {
+        Log.e("lv", "onPause")
         super.onPause()
-        stopPreview()
+        isStopPreview = true
+        camera.stopPreview()
     }
 
     override fun onDestroy() {
+        Log.e("lv", "onDestroy")
         super.onDestroy()
+        isStopPreview = true
         camera.stopPreview()
         camera.release()
     }
 
+    //SurfaceTextureListener--------------------------------start-----------------------------------
     override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) {
+        Log.e("lv", "onSurfaceTextureAvailable")
+        this.surface = surface
         camera.setDisplayOrientation(90)
         camera.setPreviewTexture(surface)
     }
 
     override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture?, width: Int, height: Int) {
-        try {
-            startPreview()
-        } catch (e: IOException) {
-            e.printStackTrace()
+        Log.e("lv", "onSurfaceTextureSizeChanged")
+    }
+
+    override fun onSurfaceTextureUpdated(surface: SurfaceTexture?) {
+        Log.e("lv", "onSurfaceTextureUpdated")
+        if (!isTakingPicture && !isStopPreview) {
+            camera.startPreview()
         }
     }
 
     override fun onSurfaceTextureDestroyed(surface: SurfaceTexture?): Boolean {
-        stopPreview()
+        Log.e("lv", "onSurfaceTextureDestroyed")
+        isStopPreview = true
         return false
     }
 
-    override fun onSurfaceTextureUpdated(surface: SurfaceTexture?) {
+    //onPreviewFrame------------------------------------start---------------------------------------
+    override fun onPreviewFrame(data: ByteArray?, camera: Camera?) {
+
     }
 
-    private fun startPreview() {
-        if (!previewIsRunning) {
+    //OnClickListener-----------------------------------start---------------------------------------
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.bt_take_picture -> takePicture()//拍照
+            R.id.bt_audio_capture -> captureVideo()//录像
+        }
+    }
+
+    /**
+     * 拍照
+     */
+    private fun takePicture() {
+        Log.e("lv", "takePicture")
+        isTakingPicture = true
+        camera.takePicture({
+            Toast.makeText(this, "拍照", Toast.LENGTH_LONG).show()
+        }, { data, camera ->
+            data?.let {
+                File(cacheDir, "picture_${System.currentTimeMillis()}.raw").writeBytes(data)
+                camera.startPreview()
+            }
+        }, { data, camera ->
+            File(cacheDir, "picture${System.currentTimeMillis()}.jpeg").writeBytes(data)
             camera.startPreview()
-            previewIsRunning = true
+            isTakingPicture = false
+        })
+    }
+
+    /**
+     * 录像
+     */
+    private fun captureVideo() {
+
+        if (!isRecording) {
+            stopCapture()
+        } else {
+            startCapture()
         }
     }
 
-    private fun stopPreview() {
-        if (previewIsRunning ) {
-            camera.stopPreview()
-            previewIsRunning = false
+    private fun startCapture() {
+        isRecording = true
+        bt_capture_video.text = getString(R.string.stop_capture)
+        camera.unlock()
+        with(mediaRecorder) {
+            setCamera(camera)
+            setAudioSource(MediaRecorder.AudioSource.CAMCORDER)
+            setVideoSource(MediaRecorder.VideoSource.CAMERA)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setVideoEncoder(MediaRecorder.VideoEncoder.H263)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setProfile(CamcorderProfile.get(QUALITY_HIGH))
+            setOutputFile("${cacheDir.absolutePath} video${System.currentTimeMillis()}.mp4")
+//            setPreviewDisplay(this@ActivityVideoCapture.surface.)
+            prepare()
+            Thread {
+                start()
+            }.start()
         }
+    }
+
+    private fun stopCapture() {
+        isRecording = false
+        bt_capture_video.text = getString(R.string.video_capture)
+        with(mediaRecorder) {
+            stop()
+            release()
+        }
+
     }
 
 }
--- app/src/main/java/com/example/avdemo/ui/video/capture/ActivityVideoCapture21.kt
+++ app/src/main/java/com/example/avdemo/ui/video/capture/ActivityVideoCapture21.kt
@@ -1,11 +1,24 @@
 package com.example.avdemo.ui.video.capture
 
+import android.annotation.SuppressLint
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.*
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import android.util.Size
+import android.view.Surface
+import android.view.TextureView
+import androidx.annotation.RequiresApi
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.example.avdemo.R
 import com.example.avdemo.common.ViewPathConst
-import com.example.avdemo.ui.record.capture.ActivityAudioRecord
+import kotlinx.android.synthetic.main.activity_video_capture21.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * desc: 21以上视频采集 <br/>
@@ -13,15 +26,157 @@ import com.example.avdemo.ui.record.capture.ActivityAudioRecord
  * author: 吕昊臻 <br/>
  * since V 1.0 <br/>
  */
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 @Route(path = ActivityVideoCapture21.Target)
-class ActivityVideoCapture21 : AppCompatActivity() {
+class ActivityVideoCapture21 : AppCompatActivity(), TextureView.SurfaceTextureListener {
+
+
+    private lateinit var cameraCaptureSessions: CameraCaptureSession
+    private lateinit var captureRequestBuilder: CaptureRequest.Builder
+    private lateinit var cameraId: String
+    private lateinit var cameraManager: CameraManager
+    private lateinit var imageDimension: Size
+    private lateinit var cameraDevice: CameraDevice
+    private lateinit var mBackgroundHandler: Handler
+    private val mBackgroundThread by lazy { HandlerThread("Camera Background") }
 
     companion object {
         const val Target = ViewPathConst.ACTIVITY_VIDEO_CPATURE21
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.e("lv","ActivityVideoCapture21--->onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_capture21)
+        tv_camera_preview.surfaceTextureListener = this
+        startBackgroundThread()
     }
+
+    override fun onResume() {
+        Log.e("lv","ActivityVideoCapture21--->onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.e("lv","ActivityVideoCapture21--->onPause")
+        stopBackgroundThread()
+        super.onPause()
+    }
+
+    private fun startBackgroundThread() {
+        mBackgroundThread.start()
+        mBackgroundHandler = Handler(mBackgroundThread.looper)
+    }
+
+    private fun stopBackgroundThread() {
+        mBackgroundThread.quitSafely()
+        try {
+            mBackgroundThread.join()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture?, width: Int, height: Int) {
+        Log.e("lv","TextureView--->onSurfaceTextureSizeChanged")
+
+    }
+
+    override fun onSurfaceTextureUpdated(surface: SurfaceTexture?) {
+        Log.e("lv","TextureView--->onSurfaceTextureUpdated")
+    }
+
+    override fun onSurfaceTextureDestroyed(surface: SurfaceTexture?): Boolean {
+        Log.e("lv","TextureView--->onSurfaceTextureDestroyed")
+        return false
+    }
+
+    override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) {
+        Log.e("lv","TextureView--->onSurfaceTextureAvailable")
+        /** 1 */
+        openCamera()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun openCamera() {
+        cameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
+        cameraId = cameraManager.cameraIdList[0]
+        val characteristics = cameraManager.getCameraCharacteristics(cameraId)
+        val map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+        imageDimension = map!!.getOutputSizes(SurfaceTexture::class.java)[0]
+        cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback() {
+            override fun onOpened(camera: CameraDevice) {
+                Log.e("lv","CameraDevice--->onOpened")
+                cameraDevice = camera
+                /** 2 */
+                createCameraPreview()
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+                Log.e("lv","CameraDevice--->onDisconnected")
+                cameraDevice.close()
+            }
+
+            override fun onError(camera: CameraDevice, error: Int) {
+                Log.e("lv","CameraDevice--->onError")
+                cameraDevice.close()
+            }
+        }, null)
+    }
+
+    private fun createCameraPreview() {
+        val surfaceTexture = tv_camera_preview.surfaceTexture
+        surfaceTexture.setDefaultBufferSize(imageDimension.width, imageDimension.height)
+        val surface = Surface(surfaceTexture)
+        captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+        captureRequestBuilder.addTarget(surface)
+        cameraDevice.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
+
+            override fun onConfigured(session: CameraCaptureSession) {
+                Log.e("lv","CameraCaptureSession--->onConfigured")
+                cameraCaptureSessions = session
+                /** 3 */
+                updatePreview()
+            }
+
+            override fun onConfigureFailed(session: CameraCaptureSession) {
+                Log.e("lv","CameraCaptureSession--->onConfigureFailed")
+
+            }
+        }, null)
+    }
+
+    private fun updatePreview() {
+        captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO)
+        cameraCaptureSessions.setRepeatingRequest(captureRequestBuilder.build(), object :CameraCaptureSession.CaptureCallback(){
+            override fun onCaptureStarted(session: CameraCaptureSession, request: CaptureRequest, timestamp: Long, frameNumber: Long) {
+                Log.e("lv","CaptureCallback--->onCaptureStarted")
+            }
+
+            override fun onCaptureProgressed(session: CameraCaptureSession, request: CaptureRequest, partialResult: CaptureResult) {
+                Log.e("lv","CaptureCallback--->onCaptureProgressed")
+            }
+
+            override fun onCaptureSequenceAborted(session: CameraCaptureSession, sequenceId: Int) {
+                Log.e("lv","CaptureCallback--->onCaptureSequenceAborted")
+            }
+
+            override fun onCaptureBufferLost(session: CameraCaptureSession, request: CaptureRequest, target: Surface, frameNumber: Long) {
+                Log.e("lv","CaptureCallback--->onCaptureBufferLost")
+            }
+
+            override fun onCaptureCompleted(session: CameraCaptureSession, request: CaptureRequest, result: TotalCaptureResult) {
+                Log.e("lv","CaptureCallback--->onCaptureCompleted")
+            }
+
+            override fun onCaptureFailed(session: CameraCaptureSession, request: CaptureRequest, failure: CaptureFailure) {
+                Log.e("lv","CaptureCallback--->onCaptureFailed")
+            }
+
+            override fun onCaptureSequenceCompleted(session: CameraCaptureSession, sequenceId: Int, frameNumber: Long) {
+                Log.e("lv","CaptureCallback--->onCaptureSequenceCompleted")
+            }
+        }, mBackgroundHandler)
+    }
+
 }
